4efb3689aa50566f15896f971e0d1baf1d65eef3
arnonJ <arnon@campus.technion.ac.il>
2022-07-04 00:35:08 +0300

    Final version as submitted

--- hw3/hw3/answers.py
+++ hw3/hw3/answers.py
@@ -53,35 +53,42 @@ def part1_generation_params():
 part1_q1 = r"""
 **Your answer:**
 
-An iteration over a very large text requires 
+To create a training set of many samples, rather than just one long sample. 
+Learning with just one train sample is not good, 
+because the training set should have various samples different from one another. 
 
-To create a training set of many samples, rather than just one sample. 
-Afterwards, in every epoch will have many iterations, each on a batch of few samples.
-
-Also, making the samples smaller allows the hidden state to cost less space.  
+Also, making the samples short allows the hidden state to cost less space.  
 
 """
+
 
 part1_q2 = r"""
 **Your answer:**
 
-The hidden state. ...
+The generator is not trained by memorizing the train set, but by learning a function to predict the next character.  
+The hidden state is learnt on multiple epochs, so it knows how to respond to different inputs.   
 
 """
+
 
 part1_q3 = r"""
 **Your answer:**
 
-So that we'll have the same order in every iteration. 
+So that we'll have the same order in every iteration.
+It matters, for the hidden state to learn from all batches.
 
 """
 
 part1_q4 = r"""
 **Your answer:**
 
-1. To make the distribution less uniform
-2. For high temperature, the distribution is less uniform
-3. For low temperature, the distribution is more uniform 
+1. To make the generated text more conservative and make real words, not too random with non-existent words.
+
+2. For high temperature, the generated distribution is more uniform. 
+So the generator chooses a random letter at every iteration.    
+
+3. For low temperature, the generated distribution is more like the learnt distribution. 
+So the generator chooses letters to make sequences that are likely to be drawn from the target distribution.  
 
 """
 # ==============
@@ -95,7 +102,7 @@ PART2_CUSTOM_DATA_URL = None
 
 def part2_vae_hyperparams():
     hypers = dict(
-        batch_size=0, h_dim=0, z_dim=0, x_sigma2=0, learn_rate=0.0, betas=(0.0, 0.0),
+        batch_size=0, h_dim=0, z_dim=0, x_sigma2=0.0, learn_rate=0.0, betas=(0.0, 0.0),
     )
     # TODO: Tweak the hyperparameters to generate a former president.
     # ====== YOUR CODE: ======
@@ -108,55 +115,48 @@ def part2_vae_hyperparams():
     # ========================
     return hypers
 
+
 part2_q1 = r"""
-**Your answer:**
+It determines the variance of the generated images, 
+how creative the model should be in generating different images.  
 
+Low values lead to a model that generates images very similar to the average input image. 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+Too high values would lead to a model that generated images that are different from the input. 
 
 """
 
 part2_q2 = r"""
 **Your answer:**
 
+1. 
+The KL-divergence loss penalizes the differences between the train and the generated distributions 
+in latent space,  
+in order to learn the train distribution. 
+The reconstruction loss penalizes the difference between the input image and the encoded-decoded image, 
+in order to learn a latent space that represents the input well. 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+2. 
+It becomes a better estimation of the input distribution. 
+
+3. It allows the model to generate new images from the input distributions, 
+that are not identical to the train set. 
+
 
 """
 
 part2_q3 = r"""
 **Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+In order to start by learning a good latent space, 
+before learning the generated distribution. 
 
 """
 
 part2_q4 = r"""
 **Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+To make it more robust against small changes. 
 
 """
 
@@ -210,39 +210,43 @@ def part3_gan_hyperparams():
 part3_q1 = r"""
 **Your answer:**
 
+The gradients are used when training the generator, and not when training the discriminator.  
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+The generator is trained to generate better samples, by gradient descent optimizers 
+with the gradient of the generated images. 
+
+The discriminator is trained to make better predictions against a given generator, by gradient descent optimizers 
+with the gradient of the predictions. 
+The gradient of the generated samples does not affect it. 
+It treats the generator as given, frozen. 
+The gradient of the predictions does not depend in the above mentioned gradient of the generated samples. 
 
 """
 
 part3_q2 = r"""
 **Your answer:**
 
+1. No. The discriminator might improve, and consequentially the generator loss will be estimated better, 
+allowing the generator to continue improving. 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+2. It is not a good sign. 
+It means that the discriminator stopped improving, and consequentially the generator loss is not well estimated. 
+The generator will learn to trick the current discriminator, and won't generalize.  
+The advantage of GAN being adversarial works only if both continue to improve.  
 """
 
 part3_q3 = r"""
 **Your answer:**
 
+The GAN results are much more varied, colorful and detailed, 
+whereas the VAE results are all similar, all blurry and with few interesting details. 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+This is because VAE is trained to learn the train set's distribution,
+and its best result would be an average of the training images with some little variance. 
+
+GAN is trained to provide variance that might risk differing from the train set, but 
+safely enough to make a well trained discriminator believe it is genuine.  
+
 
 """
 
